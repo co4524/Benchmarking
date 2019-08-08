f45aa9ea8d6b4246a4790b26275823a160416660
yenchihliao <jeff.yenchih.liao@gmail.com>
2019-08-08 16:28:56 +0800

    add simplest python abci

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Benchmarking
 benchmarking
+## Tendermint with simplest abci
+There are three ways to implemnet ABCI
+* In-process
+* ABCI-socket
+* GRPC
+t-tendermint/abci.py implements the ABCI-socket method by listening to \[port\]\(26658 by default\) by running
+```
+python abci.py [port]
+```
+
+
 # HackMD
 https://hackmd.io/SbNY5nNjQPe2CC7IC_sUMw?view
+
---
+++ t-tendermint/abci.py
@@ -0,0 +1,153 @@
+import os
+import requests
+import sys
+import time
+from web3.auto import w3
+from ethereum import utils
+
+import rlp
+from rlp.sedes import List, CountableList
+
+from threading import Thread
+
+from abci import (
+    ABCIServer,
+    BaseApplication,
+    ResponseInfo,
+    ResponseInitChain,
+    ResponseCheckTx, ResponseDeliverTx,
+    ResponseQuery,
+    ResponseCommit,
+    CodeTypeOk,
+)
+
+operator_key = '0xa18969817c2cefadf52b93eb20f917dce760ce13b2ac9025e0361ad1e7a1d448'
+operator_normalize_key = utils.normalize_key(operator_key)
+authority = utils.privtoaddr(operator_key)
+authority_address = w3.toChecksumAddress('0x' + authority.hex())
+
+
+class RequestFailedException(Exception):
+    """request failed without success http status"""
+
+
+def request(end_point, method, params=None, data=None, headers=None):
+    url = 'http://127.0.0.1:8546' + end_point
+
+    response = requests.request(
+        method=method,
+        url=url,
+        params=params,
+        data=data,
+        headers=headers,
+    )
+
+    if response.ok:
+        return response
+    else:
+        raise RequestFailedException(
+            'failed reason: {}, text: {}'.format(response.reason, response.text)
+        )
+
+
+def sign_apphash(apphash):
+    sedes = CountableList(List([rlp.sedes.binary,
+                                rlp.sedes.big_endian_int,
+                                rlp.sedes.big_endian_int,
+                                rlp.sedes.binary,
+                                rlp.sedes.binary]))
+
+    decoded = rlp.decode(bytes.fromhex(apphash), sedes)
+
+    for i in decoded:
+        data = list(i)
+        data[2] = bool(data[2])
+        msg_hash = w3.soliditySha3(['bytes32', 'uint256', 'bool', 'bytes', 'bytes'], data).hex()[2:]
+        signed = utils.ecsign(bytes.fromhex(msg_hash), operator_normalize_key)
+        tx = '1{0:0{1}X}{2:0{3}X}{4:0{5}X}{6:X}'.format(signed[1], 64,
+                signed[2], 64, signed[0], 2, data[1])
+        requests.get('http://localhost:26657/broadcast_tx_async?tx="{}"'.format(tx))
+
+
+class PlasmaCash(BaseApplication):
+    def info(self, req) -> ResponseInfo:
+        r = ResponseInfo()
+        r.version = "1.0"
+        r.last_block_height = 0
+        r.last_block_app_hash = b''
+        return r
+
+    def init_chain(self, req) -> ResponseInitChain:
+        path1 = "./out1.txt"#"/home/caideyi/TendermintOnEvm_benchmark/data/blockCommitTime.txt"
+        path2 = "./out2.txt"#"/home/caideyi/TendermintOnEvm_benchmark/data/blockTxNum.txt"
+        try:
+            os.remove(path1)
+        except:
+            print("no {0}".format(path1))
+        try:
+            os.remove(path2)
+        except:
+            print("no {0}".format(path2))
+
+        self.f1 = open(path1, 'a')
+        self.f2 = open(path2, 'a')
+        self.last_block_height = 0
+        self.txIndex = 0
+        return ResponseInitChain()
+
+    def check_tx(self, tx) -> ResponseCheckTx:
+        '''
+        end_point = '/check_tx'
+        data = {'tx': tx}
+        try:
+            request(end_point, 'POST', data=data)
+            return ResponseCheckTx(code=CodeTypeOk)
+        except Exception as e:
+            print(e)
+            return ResponseCheckTx(code=1)
+        '''
+        return ResponseCheckTx(code=CodeTypeOk)
+    
+    def deliver_tx(self, tx) -> ResponseDeliverTx:
+        '''
+        end_point = '/send_tx'
+        data = {'tx': tx}
+        try:
+            request(end_point, 'POST', data=data)
+            return ResponseDeliverTx(code=CodeTypeOk)
+        except Exception as e:
+            print(e)
+            return ResponseDeliverTx(code=1)
+        '''
+        self.txIndex += 1
+        return ResponseDeliverTx(code=CodeTypeOk)
+
+    def query(self, req) -> ResponseQuery:
+        return ResponseQuery(code=CodeTypeOk, value='0', height=self.last_block_height)
+
+    def commit(self) -> ResponseCommit:
+        '''
+        end_point = '/operator/commit'
+        ret = request(end_point, 'POST')
+        print(ret.text)
+        worker = Thread(target=sign_apphash, args=[ret.text])
+        worker.start()
+        return ResponseCommit(data=bytes.fromhex(ret.text))
+        '''
+        if self.txIndex != 0:
+            self.f1.write(format(int(time.time()*1000)))
+            self.f2.write(format(self.txIndex))
+        ret = [self.txIndex]
+        self.txIndex = 0
+        return ResponseCommit(data=bytes(ret))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        port = sys.argv[1]#26658
+    else:
+        port = 26658
+    if len(sys.argv) == 2:
+        port = int(sys.argv[1])
+    app = ABCIServer(port=port, app=PlasmaCash())
+    app.run()
